edb533f6391a2bd1d98b786917d9be7e47ca736d
Hunter Estes <esteshunter22@gmail.com>
2022-10-24 13:37:12 -0500

    added tailwind

---
+++ src/components/Modal.js
@@ -0,0 +1,18 @@
+// This component does two things: it should receive a bool value and text to be displayed
+
+import React, { useState, useEffect } from 'react';
+function Modal(props) {
+	const [message] = useState(props.message);
+	const [show, setShow] = useState(props.show);
+	useEffect(() => {
+		setTimeout(() => {
+			setShow(false);
+		}, 3000);
+	});
+	return (
+		<div className={show ? 'modal display-block' : 'modal display-none'}>
+			<section className='modal-main'>{message}</section>
+		</div>
+	);
+}
+export default Modal;
