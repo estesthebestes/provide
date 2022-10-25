ad838caad4f15a1fd8afbb8fbc23ed15b10a85f0
Hunter Estes <esteshunter22@gmail.com>
2022-10-25 09:35:22 -0500

    added storage and auth
    was working with aws to get some storage and auth going. Working on getting images to display and I want to add a couple of images to the db

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,19 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "provide": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s3providestorage607096d9": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
